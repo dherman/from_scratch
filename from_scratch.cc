6fe53b564df2ed992177c515868b75c8b47952cf
Dave Herman <dherman@mozilla.com>
2015-09-03 14:27:16 -0700

    down to one line to uncomment to demo the link error

--- from_scratch.cc
+++ from_scratch.cc
@@ -1,17 +1,16 @@
 #include <nan.h>
+#include <v8.h>
 
 extern "C" double make_a_pi();
+extern "C" void method_in_rust_make_a_pi(Nan::FunctionCallbackInfo<v8::Value> *info);
 
-// Uncommenting this causes a linker error when doing cargo build --release on root crate
-// extern "C" void method_in_rust_make_a_pi(v8::FunctionCallbackInfo *info);
-
-// This works:
-NAN_METHOD(method_make_a_pi) {
-  info.GetReturnValue().Set(make_a_pi());
-}
+// This version was working, instead of method_in_rust_make_a_pi:
+// NAN_METHOD(method_make_a_pi) {
+//   info.GetReturnValue().Set(make_a_pi());
+// }
 
 NAN_MODULE_INIT(init_all) {
-  Nan::Export(target, "make_a_pi", method_make_a_pi);
+  Nan::Export(target, "make_a_pi", (Nan::FunctionCallback)method_in_rust_make_a_pi);
 }
 
 NODE_MODULE(noder, init_all)
--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,9 @@ pub extern fn make_a_pi() -> f64 {
     3.14
 }
 
-// Uncommenting this causes a link error.
-/*
 #[no_mangle]
 pub extern fn method_in_rust_make_a_pi(info: &mut FunctionCallbackInfo) {
-    info.get_return_value().set_f64(make_a_pi());
+    // Uncommenting this causes a link error.
+    // info.get_return_value().set_f64(make_a_pi());
+    unimplemented!()
 }
-*/
