9e0a761449955c8a2caddfd5f3c67279532a6313
Dave Herman <dherman@mozilla.com>
2015-09-03 20:22:43 -0700

    simplify

--- src/lib.rs
+++ src/lib.rs
@@ -6,16 +6,11 @@ use nanners::*;
 use std::ffi::CString;
 
 #[no_mangle]
-pub extern fn make_a_pi() -> f64 {
-    3.14
-}
-
-#[no_mangle]
-pub extern fn method_make_a_pi(info: &mut FunctionCallbackInfo) {
-    info.get_return_value().set_f64(make_a_pi());
+pub extern fn make_a_pi(info: &mut FunctionCallbackInfo) {
+    info.get_return_value().set_f64(3.14);
 }
 
 #[no_mangle]
 pub extern fn init_module(mut target: LocalObject) {
-    target.export(&CString::new("make_a_pi").unwrap(), method_make_a_pi);
+    target.export(&CString::new("make_a_pi").unwrap(), make_a_pi);
 }
